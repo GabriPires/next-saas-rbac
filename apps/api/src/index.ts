4cdd87b6f3e970ca2b831b199e2e0c08720c4268
Gabriel Pires <gabpiresdev@gmail.com>
2024-04-26 15:32:20 -0300

    feat: create roles and permissions

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,4 +1,8 @@
-import { ability } from '@saas/auth'
+import { defineAbilityFor } from '@saas/auth'
+
+const ability = defineAbilityFor({
+  role: 'ADMIN',
+})
 
 const userCanInviteSomeoneElse = ability.can('delete', 'User')
 
