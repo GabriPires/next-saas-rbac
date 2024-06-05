b07b5f9eea3018fd5728f8bf06b0e061644587bb
Gabriel Pires <gabpiresdev@gmail.com>
2024-06-05 10:28:37 -0300

    feat: env variables with a new package

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -2,6 +2,7 @@ import fastifyCors from '@fastify/cors'
 import { fastifyJwt } from '@fastify/jwt'
 import { fastifySwagger } from '@fastify/swagger'
 import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { env } from '@saas/env'
 import { fastify } from 'fastify'
 import {
   jsonSchemaTransform,
@@ -42,7 +43,7 @@ app.register(fastifySwaggerUi, {
 })
 
 app.register(fastifyJwt, {
-  secret: 'my-jwt-secret',
+  secret: env.JWT_SECRET,
 })
 
 app.register(fastifyCors)
@@ -56,8 +57,8 @@ app.register(resetPassword)
 
 app
   .listen({
-    port: 3333,
+    port: env.SERVER_PORT,
   })
   .then(() => {
-    console.log('Server running on port 3333')
+    console.log(`Server running on port ${env.SERVER_PORT}`)
   })
